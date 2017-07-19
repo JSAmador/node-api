2c4360d4030036ed2582b148214079c52eeead7d
Juan Sanchez Amador <Juan.Sanchez Amador@chandlersfe.co.uk>
2017-07-19 15:54:27 +0100

    Update Configuration file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,34 +1,39 @@
 const {ObjectID} = require('mongodb');
 const jwt = require('jsonwebtoken');
+
 const {Todo} = require('./../../models/todo');
 const {User} = require('./../../models/user');
 
 const userOneId = new ObjectID();
 const userTwoId = new ObjectID();
-
 const users = [{
-  _id:userOneId,
-  email: 'test@test.com',
-  password: 'password1',
+  _id: userOneId,
+  email: 'andrew@example.com',
+  password: 'userOnePass',
   tokens: [{
     access: 'auth',
-    token: jwt.sign({_id: userOneId , access: 'auth'}, 'abc123').toString()
+    token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString()
   }]
 }, {
-  _id:userTwoId,
-  email: 'testTwo@test.com',
-  password: 'password2'
+  _id: userTwoId,
+  email: 'jen@example.com',
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString()
+  }]
 }];
-
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
@@ -38,12 +43,12 @@ const populateTodos = (done) => {
 };
 
 const populateUsers = (done) => {
-  User.remove({}).then(()=>{
-      var userOne = new User(users[0]).save();
-      var userTwo = new User(users[1]).save();
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
 
-      return Promise.all([userOne, userTwo]);
-  }).then(()=>done());
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
 };
 
 module.exports = {todos, populateTodos, users, populateUsers};
